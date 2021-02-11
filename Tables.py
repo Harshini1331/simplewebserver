07bfad7517da974341c1b7b97e61e6cd445994a3
Harshini <harshini1937@gmail.com>
2021-02-11 16:04:04 +0000

    webserver completed

--- Tables.py
+++ Tables.py
@@ -1,20 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
 from http.server import HTTPServer , BaseHTTPRequestHandler
 
 content = """
@@ -70,9 +53,3 @@ server_address = ('',80)
 httpd= HTTPServer(server_address,MyHandler)
 print("My webserver is running...")
 httpd.serve_forever()
-
-## OUTPUT:
-![output](./static/img/o1.jpg)
-
-## RESULT:
-Thus a simple webserver is developed to serve html pages and is hosted in the URL http://harshini.student.saveetha.in/. HTML code is validated.
